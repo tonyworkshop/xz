2e3d0b555d8d05293944b66a66d7735abf176995
Tony <tonyiscoding@gmail.com>
2026-01-30 11:08:00 +0800

    add images

--- update.sh
+++ update.sh
@@ -1,2 +1,4 @@
 uv run ./src/fetch_topics.py --update 30 --debug
 uv run ./src/import_data.py
+uv run ./src/download_resources.py --articles --debug
+uv run ./src/download_resources.py --images --debug
